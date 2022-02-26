4f63ab709f3408037a616e93a7627c560acb092a
Nicholas M Martin <nmmart04@louisville.edu>
2022-02-26 14:30:45 -0500

    Add project files.

---
+++ WarTracker/DataSource.cs
@@ -0,0 +1,32 @@
+﻿namespace WarTracker
+{
+    internal class DataSource : IDisposable
+    {
+        private const string START_TAG = @"<div id=""feedler"" class=""scrotabs"">";
+        private const string END_TAG = @"<div id=""language"" class=""scrotabs""";
+        private const string SOURCE_URL = "https://liveuamap.com";
+
+        private readonly HttpClient _client = new();
+
+        public async Task<string> GetAsync()
+        {
+                var result = await _client.GetAsync(SOURCE_URL);
+                var html = await result.Content.ReadAsStringAsync();
+
+                if (!result.IsSuccessStatusCode || string.IsNullOrWhiteSpace(html))
+                {
+                    throw new Exception($"No content received. Error ({result.StatusCode}) {result.ReasonPhrase}");
+                }
+
+                var startIdx = html.IndexOf(START_TAG);
+                var endIdx = html.IndexOf(END_TAG);
+
+                return html.Substring(startIdx, endIdx - startIdx);
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
---
+++ WarTracker/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+using WarTracker;
+
+const string CONTENT_FILE = "index.html";
+
+const char POSITIVE = '+';
+const char NEGATIVE = '-';
+const char NEUTRAL = ' ';
+const char DEBUG = 'D';
+
+string? lastContent = null;
+
+Console.Title = "War Tracker";
+
+TimeSpan delayAmount;
+do
+{
+    Console.Write("Enter how often to check for updates? (ex 01:00:00 is 1 hour): ");
+} while (!TimeSpan.TryParse(Console.ReadLine(), out delayAmount));
+
+Console.Clear();
+
+Log(' ', $"Checking for updates every {delayAmount}");
+
+_ = StartBackgroundJob(delayAmount);
+
+while (true)
+{
+    Console.ReadKey(true);
+
+    if (!string.IsNullOrWhiteSpace(lastContent))
+    {
+        OpenContent();
+    }
+}
+
+async Task StartBackgroundJob(TimeSpan delay)
+{
+    var ds = new DataSource();
+    int divCount = default;
+
+    while (true)
+    {
+        try
+        {
+            lastContent = await ds.GetAsync();
+            var newDivCount = lastContent.Split("<div").Length;
+
+#if DEBUG
+            Log('D', $"Old: {divCount} | New: {newDivCount}");
+#endif
+
+            if (divCount == default)
+            {
+                divCount = newDivCount;
+                await File.WriteAllTextAsync(CONTENT_FILE, lastContent);
+                Log('+', $"Got initial content @ {DateTime.Now}");
+            }
+            else if (newDivCount != divCount)
+            {
+                divCount = newDivCount;
+                await File.WriteAllTextAsync(CONTENT_FILE, lastContent);
+                Log('+', $"New update @ {DateTime.Now}");
+            }
+        }
+        catch (Exception e)
+        {
+            Log('-', e.Message);
+        }
+
+        await Task.Delay(delay);
+    }
+}
+
+void Log(char lvl, string message)
+{
+    Console.ForegroundColor = lvl switch
+    {
+        POSITIVE => ConsoleColor.Green,
+        NEGATIVE => ConsoleColor.Red,
+        NEUTRAL => ConsoleColor.White,
+        DEBUG => ConsoleColor.DarkGray,
+        _ => Console.ForegroundColor,
+    };
+
+    Console.WriteLine($"[{lvl}] {DateTime.Now} - {message}");
+    Console.ResetColor();
+}
+
+void OpenContent() => Process.Start(new ProcessStartInfo("cmd.exe", @$"/c ""{new FileInfo(CONTENT_FILE).FullName}"""));
